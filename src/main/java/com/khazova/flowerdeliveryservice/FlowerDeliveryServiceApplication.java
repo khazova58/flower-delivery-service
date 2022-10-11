14f6ce5ebd55faa35807e576a6b0cbac0c8f9c2b
khazova58 <kjuliax@gmail.com>
2022-10-11 21:58:26 +0300

    FDS-3/создание каркаса проекта

---
+++ src/main/java/com/khazova/flowerdeliveryservice/FlowerDeliveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.khazova.flowerdeliveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerDeliveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowerDeliveryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/khazova/flowerdeliveryservice/FlowerDeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khazova.flowerdeliveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerDeliveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
