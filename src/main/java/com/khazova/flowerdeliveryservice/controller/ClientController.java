60eb657652dbdb5c92eb18e962f8d852c51d2821
khazova58 <kjuliax@gmail.com>
2022-10-17 10:23:06 +0300

    FDS-6/подключение Open API(swagger)

--- src/main/java/com/khazova/flowerdeliveryservice/controller/ClientController.java
+++ src/main/java/com/khazova/flowerdeliveryservice/controller/ClientController.java
@@ -2,12 +2,15 @@ package com.khazova.flowerdeliveryservice.controller;
 
 import com.khazova.flowerdeliveryservice.model.DTO.ClientDTO;
 import com.khazova.flowerdeliveryservice.service.clients.ClientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/client/")
+@Tag(name = "Клиент")
 public class ClientController {
 
     private final ClientService clientService;
@@ -17,6 +20,7 @@ public class ClientController {
     }
 
     @PostMapping
+    @Operation(description = "Новый клиент")
     public String newClient(ClientDTO client) {
         return "newClient";//TODO
     }
