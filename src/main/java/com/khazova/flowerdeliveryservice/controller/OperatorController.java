d9a24fa448bf21962a97ba81169be237ecb67e4c
Roma <roman.tyt@gmail.com>
2022-10-31 15:53:22 +0300

    добавлен контроллер оператора, добавлен контроллер работы оператора

--- src/main/java/com/khazova/flowerdeliveryservice/controller/OperatorController.java
+++ src/main/java/com/khazova/flowerdeliveryservice/controller/OperatorController.java
@@ -2,40 +2,42 @@ package com.khazova.flowerdeliveryservice.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
-@RequestMapping("api/v1/operators")
+@RequestMapping("api/v1/operator")
 @Tag(name = "Оператор")
-
 public class OperatorController {
 
-    @PutMapping("takeAnOrderToWork/{id}")
-    @Operation(summary = "Взять заказ в работу")
-        public String takeAnOrderToWork(@PathVariable String id) {
+        @PostMapping
+        @Operation(summary = "Создать нового оператора")
+        public String newOperator(String temp) {
             return "";
-    }
+        }
+        @DeleteMapping("/{id}")
+        @Operation(summary = "Удалить оператора с заданным идентификатором")
+        public String deleteOperator(@PathVariable String id) {
+            return "";
+        }
+        @GetMapping("/{id}")
+        @Operation(summary = "Получить оператора по индентификатору")
+        public String findOneOperator(@PathVariable String id) {
+            return "";
+        }
 
-    @PutMapping("removeAnOrderFromWork/{id}")
-    @Operation(summary = "Удалить заказ из работы")
-    public String removeAnOrderFromWork(@PathVariable String id) {
-        return "";
-    }
+        @GetMapping
+        @Operation(summary = "Получить всех операторов")
+        public String findAllOperators() {
+            return "";
+        }
 
-    @PutMapping("assignCourier/{id}")
-    @Operation(summary = "Назначить курьера")
-    public String assignCourier(@PathVariable String id) {
-        return "";
-    }
-
-    @PutMapping("addOrEditCommentToTheOrder/{id}")
-    @Operation(summary = "Добавить или изменить комментарий к заказу")
-    public String addOrEditCommentToTheOrder(@PathVariable String id) {
-        return "";
-    }
+        @PutMapping("/{id}")
+        @Operation(summary = "Обновить оператора с заданным индентификаторм")
+        public String updateOperator(@PathVariable String id) {
+            return "";
+        }
 
 }
+
 
---
+++ src/main/java/com/khazova/flowerdeliveryservice/controller/WorkOperatorController.java
@@ -0,0 +1,41 @@
+package com.khazova.flowerdeliveryservice.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/operatorWork")
+@Tag(name = "Работа оператора")
+
+public class WorkOperatorController {
+
+    @PutMapping("takeAnOrderToWork/{id}")
+    @Operation(summary = "Взять заказ в работу")
+        public String takeAnOrderToWork(@PathVariable String id) {
+            return "";
+    }
+
+    @PutMapping("removeAnOrderFromWork/{id}")
+    @Operation(summary = "Удалить заказ из работы")
+    public String removeAnOrderFromWork(@PathVariable String id) {
+        return "";
+    }
+
+    @PutMapping("assignCourier/{id}")
+    @Operation(summary = "Назначить курьера")
+    public String assignCourier(@PathVariable String id) {
+        return "";
+    }
+
+    @PutMapping("addOrEditCommentToTheOrder/{id}")
+    @Operation(summary = "Добавить или изменить комментарий к заказу")
+    public String addOrEditCommentToTheOrder(@PathVariable String id) {
+        return "";
+    }
+
+}
+
