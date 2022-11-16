b25a2642acc660701b6ab25d39b7644bca811793
khazova58 <kjuliax@gmail.com>
2022-11-16 10:53:06 +0300

    FDS-17/ исправлены замечания при PR

--- src/main/java/com/khazova/flowerdeliveryservice/controller/ClientController.java
+++ src/main/java/com/khazova/flowerdeliveryservice/controller/ClientController.java
@@ -14,8 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "api/v1/clients", produces = MediaType.APPLICATION_JSON_VALUE) //комментарий для меня (consumes = MediaType.APPLICATION_JSON_VALUE)
-@Tag(name = "Клиент")
+@RequestMapping(value = "api/v1/clients", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ClientController {
 
     private final ClientService clientService;
