e6cbaa1dc8789bfe6af8a12a7d9854d53f3449d6
Roma <roman.tyt@gmail.com>
2022-11-25 04:47:04 +0300

    sql скрипт для автоматического создания таблицы оператора объединил ветки fds-24 и fds-20

--- src/main/resources/db/changelog/tables/create-table-operators.sql
+++ src/main/resources/db/changelog/tables/create-table-operators.sql
@@ -1,6 +1,6 @@
 -- liquibase formatted sql
 
--- changeset yakhazova:create-table-operators
+-- changeset Roman:create-table-operators
 create table if not exists delivery.operators
 (
     operator_id  varchar      not null primary key,
@@ -8,4 +8,13 @@ create table if not exists delivery.operators
     last_name    varchar(150) not null,
     phone_number varchar(15)  not null,
     email        varchar(300) not null
-    )
+    );
+
+-- changeset Roman:update
+    ALTER TABLE delivery.operators ADD COLUMN test VARCHAR (20);
+
+-- changeset Roman:update1
+    ALTER TABLE delivery.operators ALTER COLUMN test RENAME TO test1;
+
+-- changeset Roman:update2
+    ALTER TABLE delivery.operators DROP COLUMN test1;
