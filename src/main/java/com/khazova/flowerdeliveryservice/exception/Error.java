c84a5e7ef797f9e585f519320a78dbf970a5b73f
khazova58 <kjuliax@gmail.com>
2022-12-14 10:55:01 +0300

    FDS-34/Выбран метод обработки exception, рефакторинг

--- src/main/java/com/khazova/flowerdeliveryservice/exception/Error.java
+++ src/main/java/com/khazova/flowerdeliveryservice/exception/Error.java
@@ -1,4 +1,4 @@
-package com.khazova.flowerdeliveryservice.exception.part2;
+package com.khazova.flowerdeliveryservice.exception;
 
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
@@ -8,7 +8,7 @@ public enum Error {
     COURIER_NOT_FOUND("Error:0101", "Курьер с id '%s' не найден", HttpStatus.NOT_FOUND),
     CLIENT_NOT_FOUND("Error:0102", "Клиент с id '%s' не найден", HttpStatus.NOT_FOUND),
     ORDER_NOT_FOUND("Error:0103", "Заказ с id '%s' не найден", HttpStatus.NOT_FOUND),
-    EMAIL_EXIST("Error:0104", "Email '%s' уже существует", HttpStatus.METHOD_NOT_ALLOWED);
+    EMAIL_EXIST("Error:0104", "Email '%s' уже существует", HttpStatus.BAD_REQUEST);
 
     private final String errorCode;
     private final String description;
--- src/main/java/com/khazova/flowerdeliveryservice/exception/ServiceException.java
+++ src/main/java/com/khazova/flowerdeliveryservice/exception/ServiceException.java
@@ -1,4 +1,4 @@
-package com.khazova.flowerdeliveryservice.exception.part2;
+package com.khazova.flowerdeliveryservice.exception;
 
 import lombok.Getter;
 
