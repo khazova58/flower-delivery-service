57dedfd832d57bb37bbafaa88755b2ef9788a5ef
Roma <roman.tyt@gmail.com>
2022-12-23 22:39:18 +0300

    FDS-38/ исправления2

--- src/main/java/com/khazova/flowerdeliveryservice/model/mapper/OperatorMapper.java
+++ src/main/java/com/khazova/flowerdeliveryservice/model/mapper/OperatorMapper.java
@@ -3,15 +3,27 @@ package com.khazova.flowerdeliveryservice.model.mapper;
 import com.khazova.flowerdeliveryservice.model.dto.OperatorDTO;
 import com.khazova.flowerdeliveryservice.model.entity.Operator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OperatorMapper {
     public OperatorDTO operatorMapToDTO(Operator operator){
         return new OperatorDTO(operator.getName(), operator.getLastName(), operator.getPhoneNumber(), operator.getEmail());
     }
 
     public Operator dtoMapToOperator(OperatorDTO operatorDTO){
-        return new Operator("", operatorDTO.getName(), operatorDTO.getLastName(), operatorDTO.getPhoneNumber(), operatorDTO.getEmail());
+        return new Operator(/*"",*/ operatorDTO.getName(), operatorDTO.getLastName(), operatorDTO.getPhoneNumber(), operatorDTO.getEmail());
     }
 
-    public OperatorMapper(){
+    public List<OperatorDTO> convertListOperatorToListOperatorDTO(List<Operator> operatorList) {
+        List<OperatorDTO> operatorDTOList = new ArrayList<>();
+        for (Operator temp : operatorList) {
+            operatorDTOList.add(operatorMapToDTO(temp));
+        }
+        return operatorDTOList;
     }
-}
+
+    public OperatorMapper() {
+        }
+
+    }
