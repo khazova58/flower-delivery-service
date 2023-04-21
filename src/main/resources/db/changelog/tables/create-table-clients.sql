7be7753c4c403628ff83c1e922f8093f9d1e6f5a
khazova58 <kjuliax@gmail.com>
2023-04-21 16:51:55 +0300

    FDS-60/Тестирование методов репозитория

--- src/main/resources/db/changelog/tables/create-table-clients.sql
+++ src/main/resources/db/changelog/tables/create-table-clients.sql
@@ -3,9 +3,10 @@
 -- changeset yakhazova:create-table-clients
 create table if not exists delivery.clients
 (
-    client_id    varchar      not null primary key,
-    name         varchar(100) not null,
-    last_name    varchar(150) not null,
-    phone_number varchar(15)  not null,
-    email        varchar(300) unique not null
+    client_id    varchar             not null primary key,
+    last_name    varchar(50)         not null,
+    first_name   varchar(50)         not null,
+    middle_name  varchar(50)         not null,
+    phone_number varchar(15)         not null,
+    email        varchar(100) unique not null
 )
